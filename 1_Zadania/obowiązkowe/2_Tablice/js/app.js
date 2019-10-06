e2d6653e677327f6516017ad524819d4869f7768
zager123412 <48321022+zager123412@users.noreply.github.com>
2019-10-06 10:50:21 +0200

    Update app.js

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,7 @@
+zad 1
+let fruits =['apple','orange','banana','peach','pear'];
+console.log(fruits[0]);
+console.log(fruits[fruits.length -1]);
+for(let count =0; count fruits.length;count++){
+    console.log(fruits[count]);
+}
