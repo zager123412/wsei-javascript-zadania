94c96a6f4105b6c05e8efca635507833129bd1d5
zager123412 <48321022+zager123412@users.noreply.github.com>
2019-12-22 10:30:23 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -4,3 +4,46 @@ document.addEventListener("DOMContentLoaded", function () {
      */
 
 });
+
+
+    //zad 0
+var elements=document.getElementsByClassName('exercise ex5')[0].getElementsByTagName('li');;
+for(i=0;i<elements.length;i++)
+{
+    if(i%2==0)
+    {
+elements[i].style.backgroundColor="green";
+    }
+
+    else { 
+        
+        continue;}
+}
+for(i=0;i<elements.length;i++)
+{
+    if(i%3==0)
+    {
+elements[i].style.textDecoration="underline overline";
+    }
+
+    else { 
+        
+        continue;}
+}
+console.log(elements);
+
+elements[4].style.fontWeight="bold";
+
+//zad 2
+document.getElementById("name").innerHTML="kamil";
+document.getElementById('fav_color').innerHTML="kamil";
+document.getElementById('fav_meal').innerHTML="kamil";
+
+
+//zad 4
+var tak=document.getElementsByClassName('exercise ex4')[0].getElementsByTagName('li');
+for(i=0;i<tak.length;i++)
+{
+tak[i].dataset.id=i+1;
+
+}
