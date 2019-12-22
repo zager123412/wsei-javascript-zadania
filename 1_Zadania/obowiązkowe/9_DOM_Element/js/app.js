d8e912302cb858d5f85042476103c7b0528c2060
zager123412 <48321022+zager123412@users.noreply.github.com>
2019-12-22 11:03:14 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -10,3 +10,37 @@ document.addEventListener("DOMContentLoaded", function () {
      */
 
 });
+function getDatasInfo(links){
+var tak=[];
+for (i=0;i<links.length;i++)
+{
+    tak.push(links[i].getAttribute("data-color"));
+}
+return tak;
+}
+console.log(getDatasInfo(document.querySelectorAll(".ex5 ul li")));
+
+//zad 3
+var element=document.getElementById('mainFooter');
+
+function getId(element){
+console.log(element.getAttribute("Id"));
+
+return element.getAttribute("Id");
+}
+getId(element);
+
+//zad 5
+var banner = document.querySelector(".ban");
+
+function getClassInfo(banner){
+    console.log(banner);
+var tak1=[];
+for(i=0;i<banner.length;i++)
+{
+    tak1.push(banner[i]);
+}
+return tak1;
+
+}
+console.log(getClassInfo());
