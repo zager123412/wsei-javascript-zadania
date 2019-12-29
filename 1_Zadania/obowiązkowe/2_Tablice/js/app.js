fccb783d689063a8624cf3853fb437c47962c369
zager123412 <48321022+zager123412@users.noreply.github.com>
2019-12-29 19:18:52 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,7 +1,60 @@
-zad 1
-let fruits =['apple','orange','banana','peach','pear'];
-console.log(fruits[0]);
-console.log(fruits[fruits.length -1]);
-for(let count =0; count fruits.length;count++){
-    console.log(fruits[count]);
+//zad 0
+function distFromAvarage(foo){
+var tab=foo;
+console.log(tab.length);
+var sum=0;
+
+for(i=0;i<tab.length;i++)
+{
+    
+sum=sum+tab[i];
 }
+
+sum=sum/tab.length;
+
+
+for(i=0;i<tab.length;i++)
+{
+    tab[i]=Math.abs(tab[i]-sum);
+}
+return tab;
+
+}
+
+            
+   distFromAvarage([1,2,3,4,5,6,7]);
+   distFromAvarage([1,1,1,1]);
+   distFromAvarage([2,8,3,7]);
+
+
+
+
+//zad 1
+var fruits =["banan","jabłko","brzoskwinia","gruszka",];
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+for(i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+    
+    
+//zad 2
+    function createArray(number) {
+    var newArray = [];
+    if (number<0||number==0)
+    {
+    return newArray;
+    }
+
+    for (var counter = 1;counter <= number ;counter++) {
+        newArray.push(counter);
+    }
+
+    return newArray;
+}
+
+console.log("tablica z liczbami do 6 = " + createArray(6));
+console.log("tablica z liczbami do 1 = " + createArray(1));
+console.log("Test dla liczby ujemnej (powinna być pusta tablica) " + createArray(-6));
+console.log("Test dla zera (powinna być pusta tablica) " + createArray(0));
+    
+    //zad 3
