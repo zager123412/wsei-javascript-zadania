88d17d44030a880dc7f3aa163e3f768041e20e1d
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-06 18:51:36 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -8,27 +8,75 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
-
-});
+    //zad 0
 function getDatasInfo(links){
 var tak=[];
-for (i=0;i<links.length;i++)
-{
+for(i=0;i<links.length;i++){
     tak.push(links[i].getAttribute("data-color"));
+}
+console.log(tak);
+return tak;
+    }
+
+//zad 1
+    var homeElement = document.getElementById("home");
+    var childElements = document.querySelector(".oferts").children;
+    var banner = document.querySelector(".ban");
+    var blocks = document.querySelectorAll(".block");
+    var links = document.querySelector(".links").children;
+
+console.log("tagName: "+homeElement.tagName+" className: "+homeElement.className);
+for(i=0;i<childElements.length;i++)
+{
+    console.log("className: "+childElements[i].className +" tagName: "+childElements[i].tagName);
+    }
+console.log("className: "+banner.className+" tagName: "+banner.tagName);
+for(i=0;i<childElements.length;i++)
+{
+    console.log("className: "+blocks[i].className+" tagName: "+blocks[i].tagName);
+    }
+for(i=0;i<childElements.length;i++)
+{
+    console.log("className: "+links[i].className+" tagName: "+links[i].tagName);
+    }
+    
+// zad 2
+var blocks = document.querySelectorAll(".block");
+for(i=0;i<blocks.length;i++)
+{
+    console.log("Innerhtml"+blocks[i].innerHTML);
+    console.log("outerhtml"+blocks[i].outerHTML);
+}
+for(i=0;i<blocks.length;i++){
+blocks[i].innerHTML="Nowa wartość diva o klasie blocks";
+}
+for(i=0;i<blocks.length;i++)
+{
+    console.log("Innerhtml"+blocks[i].innerHTML);
+    console.log("outerhtml"+blocks[i].outerHTML);
+}
+
+//zad 3
+   
+var foo=document.getElementById("mainFooter");
+
+    function getId(foo){
+       
+        return foo.id;
+    }
+    getId(foo);
+
+
+//zad 4
+var childElements = document.querySelector(".oferts").children;
+function getTags(childElements){
+var tak=[];
+for(i=0;i<childElements.length;i++){
+    tak.push(childElements[i]);
 }
 return tak;
 }
-console.log(getDatasInfo(document.querySelectorAll(".ex5 ul li")));
-
-//zad 3
-var element=document.getElementById('mainFooter');
-
-function getId(element){
-console.log(element.getAttribute("Id"));
-
-return element.getAttribute("Id");
-}
-getId(element);
+getTags(childElements);
 
 //zad 5
 var banner = document.querySelector(".ban");
@@ -44,3 +92,16 @@ return tak1;
 
 }
 console.log(getClassInfo());
+
+
+//zad 6
+var elementy=document.getElementsByTagName("nav")[0].getElementsByTagName("li");
+
+function setDataDirection(elementy){
+    for(i=0;i<elementy.length;i++){
+        elementy[i].setAttribute('data-direction','top'); //('nazwa atrybutu','tekst atrybutu')
+    }
+}
+setDataDirection(elementy);
+
+});
