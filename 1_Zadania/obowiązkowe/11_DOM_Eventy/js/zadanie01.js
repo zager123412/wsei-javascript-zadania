519c199b0c5c773fa5af376e54afd3713e60838d
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 17:57:27 +0100

    Update zadanie01.js

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -3,3 +3,6 @@ var paragraf = document.querySelector("p");
 
 menu.classList.add("menu");
 paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+
+//Aby wykryć czy dokument został wczytany, korzystam ze zdarzenia DOMContentLoaded:
+//bez dodanie tego eventu nasz skrypt działa na niewczytanym dokumencie co powoduje błąd.
