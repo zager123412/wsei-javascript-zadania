256d34025a1f6ff525d15c6e00ed1525650db807
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 19:39:12 +0100

    Update zadanie08.js

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie08.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie08.js
@@ -1,6 +1,8 @@
 <script>
 
     document.addEventListener("DOMContentLoaded",fooo);
+    document.getElementById("windowWidth").innerHTML=window.innerHeight;
+    document.getElementById("windowHeight").innerHTML=window.innerWidth;
     function fooo(){
         window.addEventListener("resize",function(){
             document.getElementById("windowWidth").innerHTML=window.innerHeight;
