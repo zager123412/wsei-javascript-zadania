97a9070d6925c7ff1f7710b6557c2dcd2d5ade31
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 18:06:20 +0100

    Update zadanie02.js

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie02.js
@@ -0,0 +1,9 @@
+<script>
+    document.addEventListener("DOMContentLoaded",fooo);
+    function fooo(){
+    var licznik=0;
+    document.getElementById("mainBtn").addEventListener("click",function(){
+console.log("Hura! Działa!");
+    });
+    }
+</script>
