af6809e2b42e80ec787043f4fd003e59def68468
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 18:39:34 +0100

    Update zadanie05.js

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie05.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie05.js
@@ -7,7 +7,7 @@ for(i=0;i<tak.length;i++){
     tak[i].addEventListener("click",foo);
 }
 function foo(){
-console.log(this);
+
 var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 this.style.backgroundColor=randomColor;
 }
