328a19795772aac6281040118c6015173e187c11
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 18:39:12 +0100

    Update zadanie05.js

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie05.js
@@ -0,0 +1,15 @@
+<script>
+document.addEventListener("DOMContentLoaded",fooo);
+function fooo(){
+
+var tak=document.getElementsByClassName("box");
+for(i=0;i<tak.length;i++){
+    tak[i].addEventListener("click",foo);
+}
+function foo(){
+console.log(this);
+var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+this.style.backgroundColor=randomColor;
+}
+}
+</script>
