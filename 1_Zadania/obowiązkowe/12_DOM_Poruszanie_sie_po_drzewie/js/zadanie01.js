269438dbaeb82ecbb687f570a78fcc36d1c114fa
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 21:03:01 +0100

    Update zadanie01.js

--- 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie01.js
@@ -1,17 +1,15 @@
 <script >
-document.getElementById("button1").addEventListener("click",function(){
-    if(this.nextElementSibling.style.visibility=="hidden"){
-this.nextElementSibling.style.visibility="visible";}
-else{this.nextElementSibling.style.visibility="hidden"}
-});
-document.getElementById("button2").addEventListener("click",function(){
-    if(this.nextElementSibling.style.visibility=="hidden"){
-this.nextElementSibling.style.visibility="visible";}
-else{this.nextElementSibling.style.visibility="hidden"}
-});
-document.getElementById("button3").addEventListener("click",function(){
-    if(this.nextElementSibling.style.visibility=="hidden"){
-this.nextElementSibling.style.visibility="visible";}
-else{this.nextElementSibling.style.visibility="hidden"}
-});
-</script>
+    var tak=document.getElementsByClassName("button");
+
+    for(i=0;i<tak.length;i++)
+    {
+      tak[i].addEventListener("click",foo);
+    }
+    function foo(){
+        if(this.nextElementSibling.style.visibility=="hidden"){
+    this.nextElementSibling.style.visibility="visible";}
+    else{this.nextElementSibling.style.visibility="hidden"}
+    }
+
+    
+    </script>
