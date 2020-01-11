a0a09f5959bc8bde5d64b3da449bac2a62880d26
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-11 20:54:47 +0100

    Update zadanie01.js

---
+++ 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie01.js
@@ -0,0 +1,17 @@
+<script >
+document.getElementById("button1").addEventListener("click",function(){
+    if(this.nextElementSibling.style.visibility=="hidden"){
+this.nextElementSibling.style.visibility="visible";}
+else{this.nextElementSibling.style.visibility="hidden"}
+});
+document.getElementById("button2").addEventListener("click",function(){
+    if(this.nextElementSibling.style.visibility=="hidden"){
+this.nextElementSibling.style.visibility="visible";}
+else{this.nextElementSibling.style.visibility="hidden"}
+});
+document.getElementById("button3").addEventListener("click",function(){
+    if(this.nextElementSibling.style.visibility=="hidden"){
+this.nextElementSibling.style.visibility="visible";}
+else{this.nextElementSibling.style.visibility="hidden"}
+});
+</script>
