b11dc6d2d059b78bba3891c50638e1a15add40d9
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-12 20:59:37 +0100

    Update zadanie04.js

---
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie04.js
@@ -0,0 +1,12 @@
+<script>
+document.getElementById("remove").addEventListener("click",foo);
+function foo(){
+    var tak=document.getElementsByClassName("list")[0];
+    tak.removeChild(tak.children[0])
+    tak.removeChild(tak.children[0])
+    tak.removeChild(tak.children[0])
+    tak.removeChild(tak.children[0])
+    
+}
+
+</script>
