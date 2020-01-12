b2e76723969c48a1736eac916f13cc90772472f2
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-12 20:06:01 +0100

    Update zadanie02.js

--- 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie02.js
@@ -1,31 +1,26 @@
 <script>
-document.getElementsByClassName("button")[0].addEventListener("click",foo);
-function foo(){
-var id=document.getElementById("orderId").value;
-var przedmiot=document.getElementById("item").value;
-var liczba=document.getElementById("quantity").value;
-var tak=document.getElementById("orders").children[0];
-
-//elementy 
-var elementTr=document.createElement("tr");
-var elementTd=document.createElement("td");
-
-//nody do elementu td
-var nodeId=document.createTextNode(id);
-var nodeP=document.createTextNode(przedmiot);
-var nodeL=document.createTextNode(liczba);
-//elementTr.appendChild(elementTd).appendChild(elementTd).appendChild(elementTd);
+    
+    document.getElementsByClassName("button")[0].addEventListener("click",foo);
+    function foo(){
+       var tab=[];
 
 
-tak.appendChild(elementTr);
+    tab.push(document.getElementById("orderId").value);
+    tab.push(document.getElementById("item").value);
+    tab.push(document.getElementById("quantity").value);
+    var tak=document.getElementById("orders").children[0];
+    if(tab[0]==""||tab[1]==""||tab[2]==""){
+        return;
+    }
+   var elementTr=document.createElement("tr");
+    tak.appendChild(elementTr);
 
-
-
-
-
-
-tak.lastChild.appendChild(elementTd.appendChild(nodeId)).parentElement.appendChild(document.createElement("td".parentElement(nodeP))).parentElement.appendChild(document.createElement("td".parentElement(nodeL)));
-
-}
-
-</script>
+    for(i=0;i<3;i++){
+        var node=document.createTextNode(tab[i]);
+        var elementTd=document.createElement("td");
+        elementTd.appendChild(node);
+        tak.lastChild.appendChild(elementTd);
+    }
+    }
+    
+    </script>
