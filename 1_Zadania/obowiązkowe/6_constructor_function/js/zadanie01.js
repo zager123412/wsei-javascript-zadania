9291e02ab1db2fcf3c4ee23897702ff2a4f08665
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-01-25 17:01:32 +0100

    Update zadanie01.js

--- 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
@@ -4,20 +4,29 @@ var Robot = function (name) {
 }
 
 Robot.prototype.sayHi = function (toWho) {
-    if (isFunctional === true) {
+    if (this.isFunctional === true) { //added this
         console.log("Robot " + this.name + " greets " + toWho);
     }
     else {
-        console.log("Robot " + this.name + "is broken");
+        console.log("Robot " + this.name + " is broken");
     }
 };
 
 Robot.prototype.changeName = function (newname) {
-    console.log("Robot " + this.name + "changes name to " + newname);
-    name = newname;
+    console.log("Robot " + this.name + " changes name to " + newname);
+    this.name = newname; //added this
 };
 
 Robot.prototype.fixIt = function () {
     this.isFunctional = true;
-    console.log("Robot " + this.name + "was fixed");
+    console.log("Robot " + this.name + " was fixed");
 };
+
+console.log("Zadanie 1:");
+var Robot1 = new Robot("Bob");
+Robot1.isFunctional = false;
+Robot1.sayHi("Jan");
+Robot1.fixIt();
+Robot1.sayHi("Kazimierz");
+Robot1.changeName("Rob");
+console.log(Robot1.name);
