2d78100d4437e99269f093e1a8474f3e0cbdff29
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-02-05 18:45:05 +0100

    Update zadanie07.js

--- 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie07.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie07.js
@@ -1,3 +1,5 @@
+///import { link } from "fs";
+console.log("Zadanie 07:");
 const App = function () {
   this.websites = ['onet', 'wp', 'facebook'];
   this.links = [];
@@ -5,10 +7,11 @@ const App = function () {
 
 
 App.prototype.generateLinks = function () {
-
+    for(let i=0; i<this.websites.length; i++)
+      this.links.push("https://"+this.websites[i]+".com");
 }
 
 
 let app = new App();
 app.generateLinks();
-console.log(app.links)
+console.log(app.links);
