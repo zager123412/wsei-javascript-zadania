74b87cda2c8ac7c443d33026d616f323841329b3
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-02-05 18:45:40 +0100

    Update zadanie08.js

--- 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie08.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie08.js
@@ -1,11 +1,21 @@
-console.log("Zadanie 08:");
-const dog = {
-    type: "Mammal",
-    name: "",
-    setName: function(newName) {
-        this.name = newName;
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const span = document.querySelectorAll("span");
+    const li = document.querySelectorAll("li");
 
-dog.setName("Reksio");
-console.log(dog.name);
+    for(let i=0; i<li.length; i++)
+        li[i].style.display = "none";
+
+    for(let j=0; j<span.length; j++) {
+        span[j].addEventListener("mouseover", () => {
+            const childs = span[j].parentElement.getElementsByTagName("li");
+            for(let k=0; k<childs.length; k++)
+                childs[k].style.display = "block";
+        })
+        span[j].addEventListener("mouseout", () => {
+            const childs = span[j].parentElement.getElementsByTagName("li");
+            for(let l=0; l<childs.length; l++)
+                childs[l].style.display = "none";
+        })
+    }
+})
