6d8ea228a6fbc32e0d4715b3c7b4172a2bb84f59
zager123412 <48321022+zager123412@users.noreply.github.com>
2020-02-05 18:49:16 +0100

    Update app.js

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,22 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("działa");
+    const tooltips = document.getElementsByClassName("tooltip");
+    console.log(tooltips);
+    let span;
+    for(let i=0; i<tooltips.length; i++) {
+        tooltips[i].addEventListener("mouseover", function() {
+            span = document.createElement("span");
+            span.className = "tooltipText";
+            span.textContent = tooltips[i].getAttribute("data-text");
+            tooltips[i].appendChild(span);
+            console.log(tooltips[i].getAttribute("data-text"));
+        })
+        tooltips[i].addEventListener("mouseout", function() {
+            tooltips[i].removeChild(span);
+            console.log("mouse out");
+        })
+    }
+})
